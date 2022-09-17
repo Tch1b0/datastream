93f79a52709fa6b5ffc05b34524602019e36ebd6
tch1b0 <jpour3006@gmail.com>
2022-09-17 19:44:39 +0200

    add python client

---
+++ client.py
@@ -0,0 +1,24 @@
+from hashlib import md5
+from websocket import create_connection
+
+ws = create_connection("ws://localhost:8080/ws")
+data = bytearray()
+print("Start receiving...")
+while ws.recv() != "EOF":
+    print("Start receiving chunk...")
+    while True:
+        content: bytearray = ws.recv() # type: ignore
+        checksum: str = ws.recv()
+        print(f"Received chunk with content \"{content}\" and checksum {checksum}")
+        if (generated_sum := md5(content).hexdigest()) != checksum:
+            print(f"Checksum {generated_sum} not matching => Request new chunk")
+            continue
+        else:
+            print("Chunk tasting good")
+            ws.send("ok")
+            data.extend(content)
+            break
+
+ws.close()
+
+print(data)
